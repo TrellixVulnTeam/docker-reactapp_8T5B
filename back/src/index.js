c8623a3057326c2c97ef605994f58e7ed110d4be
manuel <beledo.m@gmail.com>
2022-04-11 11:54:01 -0300

    working

--- back/src/index.js
+++ back/src/index.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let cors = require('cors')
 let serverRouter = require('./routes')
-let PORT = 3001
+let PORT = 5000
 let app = express();
 
 // Middlewares //
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors('*'));
 app.use('/api', serverRouter) // Access router from routes
 app.get('/', (req, res) => res.send('En la raiz del server'))
 
